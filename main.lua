d6ee4a8333d2750b7ce9daf149e3234cc237e342
hailazra <arabaniseis@gmail.com>
2025-09-02 21:20:54 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,7 @@
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/hailazra/Development/refs/heads/main/supportedgames.lua"))()
+
+local URL = Games[game.PlaceId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
