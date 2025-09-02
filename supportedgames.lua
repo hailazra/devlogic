32ee1fac763fb251cd11fab7f7a277afea22d06d
hailazra <arabaniseis@gmail.com>
2025-09-02 21:31:55 +0700

    Add files via upload

---
+++ supportedgames.lua
@@ -0,0 +1,5 @@
+local Games = {
+    [126884695634066] = "https://raw.githubusercontent.com/hailazra/Development/refs/heads/main/gag.lua", -- Grow a Garden
+    [121864768012064] = "https://raw.githubusercontent.com/hailazra/Development/refs/heads/main/fishtester.lua" --- Fish It
+}
+return Games
