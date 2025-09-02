912b5de299a97b8530ef45e2ca3e90872faa8232
hailazra <arabaniseis@gmail.com>
2025-09-02 21:52:12 +0700

    wd

--- supportedgames.lua
+++ supportedgames.lua
@@ -1,5 +1,5 @@
 local Games = {
-    [126884695634066] = "https://raw.githubusercontent.com/hailazra/Development/refs/heads/main/gag.lua", -- Grow a Garden
-    [121864768012064] = "https://raw.githubusercontent.com/hailazra/Development/refs/heads/main/fishtester.lua" --- Fish It
+    [126884695634066] = "https://raw.githubusercontent.com/hailazra/devlogic/refs/heads/main/Supported-Games/gag.lua", -- Grow A Garden
+    [121864768012064] = "https://raw.githubusercontent.com/hailazra/devlogic/refs/heads/main/Supported-Games/fishit.lua" --- Fish It
 }
 return Games
