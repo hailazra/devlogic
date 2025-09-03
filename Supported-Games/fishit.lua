bacff0811860ea1def8e83bad9c21fce63132f6f
hailazra <arabaniseis@gmail.com>
2025-09-03 21:42:02 +0700

    zd

--- Supported-Games/fishit.lua
+++ Supported-Games/fishit.lua
@@ -3,6 +3,9 @@ local WindUI = loadstring(game:HttpGet(
     "https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"
 ))()
 
+-- ===========================
+-- GLOBAL SERVICES & VARIABLES
+-- ===========================
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
