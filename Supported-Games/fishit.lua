ccaaec3dc7a89ce1037a0dd5060d01d4b951be36
hailazra <arabaniseis@gmail.com>
2025-09-03 20:34:47 +0700

    sfd

--- Supported-Games/fishit.lua
+++ Supported-Games/fishit.lua
@@ -37,7 +37,7 @@ FeatureManager.LoadedFeatures = {}
 local FEATURE_URLS = {
     AutoFish        = "https://raw.githubusercontent.com/hailazra/devlogic/refs/heads/main/Fish-It/autofish.lua", 
     AutoSellFish    = "https://raw.githubusercontent.com/hailazra/devlogic/refs/heads/main/Fish-It/autosellfish.lua", 
-    AutoTeleport    = "https://raw.githubusercontent.com/hailazra/devlogic/refs/heads/main/Fish-It/autoteleportisland.lua"
+    AutoTeleportIsland  = "https://raw.githubusercontent.com/hailazra/devlogic/refs/heads/main/Fish-It/autoteleportisland.lua"
 }
 
 function FeatureManager:LoadFeature(featureName, controls)
