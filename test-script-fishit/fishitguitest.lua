8e3f32151663bcc3942eedd3c3a79915c3a938eb
hailazra <arabaniseis@gmail.com>
2025-09-12 06:33:22 +0700

    Update fishitguitest.lua fixed

--- test-script-fishit/fishitguitest.lua
+++ test-script-fishit/fishitguitest.lua
@@ -198,7 +198,7 @@ function IconController.new(Window, opts)
         image = (opts and opts.image) or "rbxassetid://73063950477508",
         size = (opts and opts.size) or Vector2.new(44, 44),
         startPos = (opts and opts.startPos) or UDim2.new(0, 10, 0.5, -22),
-        clickThreshold = 8, -- Increased from 6
+        clickThreshold = 6, -- Increased from 6
         clickTimeLimit = 0.3, -- Increased from 0.22
         snapDistance = 15,
         
@@ -1923,6 +1923,7 @@ if type(Window.OnDestroy) == "function" then
         end
     end)
 end
+
 
 
 
